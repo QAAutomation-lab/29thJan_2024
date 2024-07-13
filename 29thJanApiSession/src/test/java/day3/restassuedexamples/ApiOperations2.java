04cd7e1879e1bb803c86d5c75f344aaed7277c99
QAAutomation-lab <automationguide13@gmail.com>
2024-07-13 08:00:52 +0530

    Ser & Deser example

---
+++ 29thJanApiSession/src/test/java/day3/restassuedexamples/ApiOperations2.java
@@ -0,0 +1,44 @@
+package day3.restassuedexamples;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class ApiOperations2 {
+
+	@Test
+	public void createUser() {
+		String payload = "{\"name\": \"morpheus\",\"job\": \"leader\"}";
+		//first form the base URL
+		RestAssured.baseURI = "https://reqres.in/";
+		//Given
+		RequestSpecification req  = RestAssured.given()
+					   .header("Content-Type", "application/json")
+					   .contentType(ContentType.JSON)
+					   .accept(ContentType.JSON)
+					   .body(payload);
+		//when
+		Response res=req.post("api/users");
+
+		String statusLine = res.getStatusLine();
+		System.out.println("Status Line: " + statusLine);
+		int statusCode = res.getStatusCode();
+		System.out.println("Status code: " + statusCode);
+		String responseBody = res.getBody().asString();
+		System.out.println("responseBody: " + responseBody);
+		
+		//then
+		res.then().statusCode(201);
+	}
+}
+/*
+Practice remaining API GET & POST
+
+*/
